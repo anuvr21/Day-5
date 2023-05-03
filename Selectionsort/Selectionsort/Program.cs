fa1db266c173bb3c8b1f2e5747fad2f2b57054f9
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 00:10:10 +0530

    Add files via upload

---
+++ Selectionsort/Selectionsort/Program.cs
@@ -0,0 +1,26 @@
+﻿//Selection Sort
+using System;
+namespace Selectionsort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input the array length
+            Console.WriteLine("Enter the length of the array:");
+            int al=int.Parse(Console.ReadLine());
+
+            //Declaring the array
+            int[] arr=new int[al];
+
+            //Adding elements to the array
+            int i;
+            for(i=0;i<al;i++)
+            {
+
+            }
+
+
+        }
+    }
+}
