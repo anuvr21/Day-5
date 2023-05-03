5fb43758d90128bf196f73bf05fe6a7ebfd1c19a
anuvr21 <129931334+anuvr21@users.noreply.github.com>
2023-05-04 00:12:48 +0530

    Add files via upload

---
+++ AccountDetails/AccountDetails/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.CodeDom;
+using System.Xml.Linq;
+
+namespace AccountDetails
+{
+    class Account
+    {
+        public int id;
+        private string _name;
+
+        public string Name
+        {
+            set {
+                _name = value;
+                
+                if (value.Length>15) 
+                {
+                    throw new ApplicationException("Name too long");
+                }
+                
+            }
+        }
+        private decimal _balance;
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            
+        }
+
+        public void CreateAccount(int id, string name, decimal balance)
+        {
+            this.id = id;
+            this._name = name;
+            this._balance = balance;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("***** Account Information *****");
+            Console.WriteLine("Account Id:" + id);
+            Console.WriteLine("Name:" + _name);
+            Console.WriteLine("Opening Balance:" + _balance);
+        }
+
+        public void Deposit(decimal amount)
+        { 
+           _balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (_balance - amount < 500)
+                throw new ApplicationException("Insufficient Balance");
+            else
+                this._balance -= amount;
+        }
+        public void CheckBalance()
+        {
+            //Console.WriteLine("Your balance is " + _balance);
+            _balance += 1000;
+        }
+    }
+}
---
+++ AccountDetails/AccountDetails/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace AccountDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            Account a = null;
+            do
+            {
+                Console.WriteLine("*********************************");
+                Console.WriteLine("*********ACCOUNT DETAILS*********");
+                Console.WriteLine("*********************************");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Display Account Information");
+                Console.WriteLine("3. Deposit Amount");
+                Console.WriteLine("4. Withdraw Amount");
+                Console.WriteLine("5. Check Balance");
+                Console.WriteLine("0. Quit");
+                Console.WriteLine("Enter your choice:");
+                ch = int.Parse(Console.ReadLine());
+
+                switch(ch)
+                {
+                    case 1: 
+                        Console.WriteLine("Enter your id:");
+                        int id=int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your name:");
+                        string name=Console.ReadLine();
+                        Console.WriteLine("Enter opening balance:");
+                        decimal balance=decimal.Parse(Console.ReadLine());
+                        a = new Account();
+                        a.CreateAccount(id,name,balance);
+                        break;
+                    case 2:
+                        if (a != null)
+                        {
+                            a.DisplayInfo();
+                        }
+                        else
+                        {
+                            Console.WriteLine("***Open Account First***");
+                        }
+                        break;
+                    case 3:
+                        if (a != null)
+                            a.Deposit(5000);
+                        else
+                            Console.WriteLine("*** Open Account First ***");
+                        break;
+                    case 4:
+                        if (a != null)
+                            a.Withdraw(5000);
+                        else
+                            Console.WriteLine("*** Open Account First ***");
+                        break;
+                    case 5:
+                        if (a != null)
+                        {
+                            a.CheckBalance();
+                        }
+                            
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("*** Wrong Choice ***");
+                        break;
+                }
+            }
+            while (ch != 0);
+            Console.WriteLine("***Thanks for using the application***");
+        }
+    }
+}
